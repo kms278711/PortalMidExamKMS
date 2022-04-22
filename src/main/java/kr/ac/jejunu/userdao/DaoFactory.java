6a2cd312205829cf052636088c247bda04439f51
kms278711 <kms278711@naver.com>
2022-04-22 16:29:47 +0900

    6번

--- src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -1,16 +1,35 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 public class DaoFactory {
+    @Value("${db.driver}")
+    private String driver;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+
     @Bean
-    public UserDao userDao() {
-        return new UserDao(connectionMaker());
+    public UserDao userDao() throws ClassNotFoundException {
+        return new UserDao(dataSource());
     }
     @Bean
-    public ConnectionMaker connectionMaker() {
-        return new JejuConnectionMaker();
+    public DataSource dataSource() throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(driver));
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
     }
 }
